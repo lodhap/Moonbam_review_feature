e6a7ac6db38f9757600cedb2b4bcad93aaa527bc
bsj4387 <7834jsb@gmail.com>
2024-03-12 19:05:34 +0900

    리뷰기능 spring-springboot로 변경

---
+++ moonbam_review_feature/src/main/java/com/moonbam/MoonbamReviewFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.moonbam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonbamReviewFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonbamReviewFeatureApplication.class, args);
+	}
+
+}
